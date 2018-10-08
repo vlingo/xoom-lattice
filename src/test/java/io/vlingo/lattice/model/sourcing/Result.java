29fdc49ce7ee250c30f9002ba6c4e79c6f1e882c
Vaughn Vernon <vaughn@forcomprehension.com>
2018-10-08 03:33:05 -0700

    Initial revision.

---
+++ src/test/java/io/vlingo/lattice/model/sourcing/Result.java
@@ -0,0 +1,20 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.model.sourcing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.vlingo.actors.testkit.TestUntil;
+
+public class Result {
+  public List<Object> applied = new ArrayList<>();
+  public boolean tested1;
+  public boolean tested2;
+  public TestUntil until = TestUntil.happenings(1);
+}
