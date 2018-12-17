652bbd80a906dd5e85e716523f9f31ac79223d9d
Vaughn Vernon <vaughn@forcomprehension.com>
2018-12-16 22:14:12 -0700

    Initial revision.

---
+++ src/test/java/io/vlingo/lattice/model/sourcing/DoCommand2Adapter.java
@@ -0,0 +1,26 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.model.sourcing;
+
+import io.vlingo.common.serialization.JsonSerialization;
+import io.vlingo.symbio.EntryAdapter;
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.Entry.TextEntry;
+
+public final class DoCommand2Adapter implements EntryAdapter<DoCommand2,TextEntry> {
+  @Override
+  public DoCommand2 fromEntry(final TextEntry entry) {
+    return JsonSerialization.deserialized(entry.entryData, DoCommand2.class);
+  }
+
+  @Override
+  public TextEntry toEntry(final DoCommand2 source) {
+    final String serialization = JsonSerialization.serialized(source);
+    return new TextEntry(Test1Happened.class, 1, serialization, Metadata.nullMetadata());
+  }
+}
