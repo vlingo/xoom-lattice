a7c7e4ee6eb48ed7dfa5c74e8514c45c37603329
Vaughn Vernon <vaughn@forcomprehension.com>
2019-01-03 17:12:39 -0700

    Added standard behaviors and adapters.

--- src/test/java/io/vlingo/lattice/model/projection/state/Entity1.java
+++ src/test/java/io/vlingo/lattice/model/projection/state/Entity1.java
@@ -7,6 +7,11 @@
 
 package io.vlingo.lattice.model.projection.state;
 
+import io.vlingo.common.serialization.JsonSerialization;
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.StateAdapter;
+import io.vlingo.symbio.State.TextState;
+
 public class Entity1 {
   public final String id;
   public final int value;
@@ -14,5 +19,47 @@ public class Entity1 {
   public Entity1(final String id, final int value) {
     this.id = id;
     this.value = value;
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * id.hashCode();
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (other == null || other.getClass() != this.getClass()) {
+      return false;
+    }
+    return this.id.equals(((Entity1) other).id);
+  }
+
+  @Override
+  public String toString() {
+    return "Entity1[id=" + id + " value=" + value + "]";
+  }
+
+  public static class Entity1StateAdapter implements StateAdapter<Entity1,TextState> {
+
+    @Override
+    public int typeVersion() {
+      return 1;
+    }
+
+    @Override
+    public Entity1 fromRawState(final TextState raw) {
+      return JsonSerialization.deserialized(raw.data, raw.typed());
+    }
+
+    @Override
+    public TextState toRawState(final Entity1 state, final int stateVersion) {
+      return toRawState(state, stateVersion, Metadata.with("value", "op"));
+    }
+
+    @Override
+    public TextState toRawState(final Entity1 state, final int stateVersion, final Metadata metadata) {
+      final String serialization = JsonSerialization.serialized(state);
+      return new TextState(state.id, Entity1.class, typeVersion(), serialization, stateVersion, metadata);
+    }
   }
 }
