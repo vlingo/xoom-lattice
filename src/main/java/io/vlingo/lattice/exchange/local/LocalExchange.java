f23182a27de1819d9ec83c5982a91d54d04c1093
VaughnVernon <vaughn@forcomprehension.com>
2019-03-16 21:26:41 -0700

    Initial revision of LocalExchange.

---
+++ src/main/java/io/vlingo/lattice/exchange/local/LocalExchange.java
@@ -0,0 +1,67 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.exchange.local;
+
+import io.vlingo.common.message.Message;
+import io.vlingo.common.message.MessageQueue;
+import io.vlingo.common.message.MessageQueueListener;
+import io.vlingo.lattice.exchange.Covey;
+import io.vlingo.lattice.exchange.Exchange;
+import io.vlingo.lattice.exchange.Forwarder;
+
+public class LocalExchange implements Exchange, MessageQueueListener {
+  private static final Object Channel = new Object();
+  private static final Object Connection = new Object();
+
+  private final MessageQueue queue;
+  private final Forwarder forwarder;
+
+  public LocalExchange(final MessageQueue queue) {
+    this.queue = queue;
+    queue.registerListener(this);
+    this.forwarder = new Forwarder();
+  }
+
+  @Override
+  public void close() {
+    queue.close(true);
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Object channel() {
+    return Channel;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Object connection() {
+    return Connection;
+  }
+
+  @Override
+  public String name() {
+    return "LocalExchange";
+  }
+
+  @Override
+  public <L,E,EX> Exchange register(final Covey<L,E,EX> covey) {
+    forwarder.register(covey);
+    return this;
+  }
+
+  @Override
+  public <L> void send(final L local) {
+    forwarder.forwardToSender(local);
+  }
+
+  @Override
+  final public void handleMessage(final Message message) throws Exception {
+    forwarder.forwardToReceiver(message);
+  }
+}
---
+++ src/main/java/io/vlingo/lattice/exchange/local/LocalExchangeMessage.java
@@ -0,0 +1,25 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.exchange.local;
+
+import io.vlingo.common.message.Message;
+
+public class LocalExchangeMessage implements Message {
+  public final String type;
+  public final Object payload;
+
+  public LocalExchangeMessage(final String type, final Object payload) {
+    this.type = type;
+    this.payload = payload;
+  }
+
+  @Override
+  public String toString() {
+    return "LocalExchangeMessage[type=" + type + " payload=" + payload + "]";
+  }
+}
