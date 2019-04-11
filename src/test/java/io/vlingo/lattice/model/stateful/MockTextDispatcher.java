d32a9f3344f6524856818d167ad5940c5984e12c
VaughnVernon <vaughn@forcomprehension.com>
2019-04-11 12:45:34 +0200

    Support Entry dispatching.

--- src/test/java/io/vlingo/lattice/model/stateful/MockTextDispatcher.java
+++ src/test/java/io/vlingo/lattice/model/stateful/MockTextDispatcher.java
@@ -10,20 +10,23 @@ package io.vlingo.lattice.model.stateful;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import io.vlingo.actors.testkit.TestUntil;
-import io.vlingo.symbio.Source;
+import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.symbio.Entry;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.state.StateStore.Dispatcher;
 import io.vlingo.symbio.store.state.StateStore.DispatcherControl;
 
 public class MockTextDispatcher implements Dispatcher {
+  private AccessSafely access = AccessSafely.afterCompleting(0);
+
   // public final ConfirmDispatchedResultInterest confirmDispatchedResultInterest;
   public DispatcherControl control;
   public final Map<String,State<?>> dispatched = new HashMap<>();
+  private final ConcurrentLinkedQueue<Entry<?>> dispatchedEntries = new ConcurrentLinkedQueue<>();
   public final AtomicBoolean processDispatch = new AtomicBoolean(true);
-  public TestUntil until = TestUntil.happenings(0);
 
   public MockTextDispatcher() {
   }
@@ -34,10 +37,38 @@ public class MockTextDispatcher implements Dispatcher {
   }
 
   @Override
-  public <S extends State<?>, C extends Source<?>> void dispatch(final String dispatchId, final S state, final Collection<C> sources) {
+  public <S extends State<?>, E extends Entry<?>> void dispatch(final String dispatchId, final S state, final Collection<E> entries) {
     if (processDispatch.get()) {
-      dispatched.put(dispatchId, state);
-      until.happened();
+      access.writeUsing("dispatched", dispatchId, new Dispatch<>(state, entries));
+    }
+  }
+
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  public AccessSafely afterCompleting(final int times) {
+    access = AccessSafely
+      .afterCompleting(times)
+
+      .writingWith("dispatched", (String id, Dispatch dispatch) -> { dispatched.put(id, dispatch.state); dispatchedEntries.addAll(dispatch.entries); })
+
+      .readingWith("dispatchedIds", () -> dispatched.keySet())
+      .readingWith("dispatchedState", (String id) -> dispatched.get(id))
+      .readingWith("dispatchedStateCount", () -> dispatched.size())
+
+      .writingWith("processDispatch", (Boolean flag) -> processDispatch.set(flag))
+      .readingWith("processDispatch", () -> processDispatch.get())
+
+      .readingWith("dispatched", () -> dispatched);
+
+    return access;
+  }
+
+  private static class Dispatch<S extends State<?>,E extends Entry<?>> {
+    final Collection<E> entries;
+    final S state;
+
+    Dispatch(final S state, final Collection<E> entries) {
+      this.state = state;
+      this.entries = entries;
     }
   }
 }
