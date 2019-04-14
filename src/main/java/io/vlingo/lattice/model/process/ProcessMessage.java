c7ee943550bf3309d47069a2a1f705dfe9184d74
VaughnVernon <vaughn@forcomprehension.com>
2019-04-14 10:34:21 -0700

    Implmented id(), sourceType(), typedSource().

--- src/main/java/io/vlingo/lattice/model/process/ProcessMessage.java
+++ src/main/java/io/vlingo/lattice/model/process/ProcessMessage.java
@@ -48,13 +48,51 @@ public class ProcessMessage extends Source<ProcessMessage> {
     this.source = source;
   }
 
+  /**
+   * Construct my default state with {@code null} and a type version of 1.
+   */
   public ProcessMessage() {
     super();
 
     this.source = null;
   }
 
+  /**
+   * Answer the {@code id} of my {@code source}. For use by a
+   * {@code Process}, every ProcessMessage must provide a valid
+   * {@code id} for workflow correlation, and this {@code id}
+   * must match the {@code id} of the {@code Process}.
+   * @return String
+   * @see io.vlingo.symbio.Source#id()
+   */
+  @Override
+  public String id() {
+    return source.id();
+  }
+
+  /**
+   * Answer the type of my {@code source} as a {@code Class<?>}.
+   * @return {@code Class<?>}
+   */
+  public Class<?> sourceType() {
+    return source.getClass();
+  }
+
+  /**
+   * Answer the type name of my {@code source} as a {@code String}.
+   * @return String
+   */
   public String sourceTypeName() {
     return source.getClass().getName();
+  }
+
+  /**
+   * Answer my {@code source} as an instance of type {@code T}.
+   * @param <T> the type of Source
+   * @return T
+   */
+  @SuppressWarnings("unchecked")
+  public <T extends Source<?>> T typedSource() {
+    return (T) source;
   }
 }
