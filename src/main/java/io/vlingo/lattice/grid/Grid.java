04a3b4d639270eaefd12064c57ae4a222c456349
VaughnVernon <vaughn@forcomprehension.com>
2019-04-14 10:31:52 -0700

    More complete but temporary implementations.

--- src/main/java/io/vlingo/lattice/grid/Grid.java
+++ src/main/java/io/vlingo/lattice/grid/Grid.java
@@ -7,11 +7,15 @@
 
 package io.vlingo.lattice.grid;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.Address;
 import io.vlingo.actors.AddressFactory;
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.Stage;
+import io.vlingo.common.Completes;
 import io.vlingo.common.identity.IdentityGeneratorType;
 import io.vlingo.lattice.grid.cache.Cache;
 import io.vlingo.lattice.grid.cache.CacheNodePoint;
@@ -31,14 +35,38 @@ public class Grid {
     return new Grid(stage, type);
   }
 
-  public <T> T actorFor(final Definition definition, final Class<T> protocol) {
+  public <T> T actorFor(final Class<T> protocol, final Definition definition) {
+    return actorFor(protocol, definition, addressFactory.unique());
+  }
+
+  public <T> T actorFor(final Class<T> protocol, final Definition definition, final Address address) {
+    if (stage.world().isTerminated()) {
+      throw new IllegalStateException("vlingo/lattice: Grid has stopped.");
+    }
+    if (!address.isDistributable()) {
+      throw new IllegalArgumentException("Address is not distributable.");
+    }
+
+    final T actor = stage.actorFor(protocol, definition, address);
+
+    return actor;
+  }
+
+  public <T> T actorFor(final Class<T> protocol, final Class<? extends Actor> type, final Object...parameters) {
     if (stage.world().isTerminated()) {
       throw new IllegalStateException("vlingo/lattice: Grid has stopped.");
     }
 
-    final T actor = stage.actorFor(protocol, definition);
+    final T actor = stage.actorFor(protocol, Definition.has(type, Arrays.asList(parameters)), addressFactory.unique());
 
     return actor;
+  }
+
+  public <T> Completes<T> actorOf(final Class<T> protocol, final Address address) {
+    if (!address.isDistributable()) {
+      throw new IllegalArgumentException("Address is not distributable.");
+    }
+    return stage.actorOf(protocol, address);
   }
 
   private Grid(final Stage stage, final IdentityGeneratorType type) {
