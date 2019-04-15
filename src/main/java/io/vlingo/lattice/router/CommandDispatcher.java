9437efb98069d3a3143dc146786ed4d239c963b5
VaughnVernon <vaughn@forcomprehension.com>
2019-04-14 19:18:25 -0700

    Inital revision of CommandDispatcher functional interface.

---
+++ src/main/java/io/vlingo/lattice/router/CommandDispatcher.java
@@ -0,0 +1,15 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.router;
+
+import io.vlingo.lattice.model.Command;
+
+@FunctionalInterface
+public interface CommandDispatcher<P,C extends Command,A> {
+  void accept(final P protocol, final C command, final A answer);
+}
