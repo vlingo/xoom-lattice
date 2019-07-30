cdbedd503635c3c161af10c3b6a716e777572075
VaughnVernon <vaughn@forcomprehension.com>
2019-07-29 23:55:59 -0700

    Initial revision of feed.Feed.

---
+++ src/test/java/io/vlingo/lattice/exchange/feed/MockFeedConsumer.java
@@ -0,0 +1,33 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.exchange.feed;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import io.vlingo.actors.testkit.AccessSafely;
+
+public class MockFeedConsumer implements FeedConsumer {
+  private AccessSafely access;
+  public final Map<Long,FeedItem> feedItems = new ConcurrentHashMap<>();
+
+  @Override
+  public void consumeFeedItem(final FeedItem feedItem) {
+    access.writeUsing("feedItems", feedItem);
+  }
+
+  public AccessSafely afterCompleting(final int times) {
+    access = AccessSafely
+      .afterCompleting(times)
+
+      .writingWith("feedItems", (FeedItem feedItem) -> feedItems.put(feedItem.id.toLong(), feedItem))
+      .readingWith("feedItems", () -> feedItems);
+
+    return access;
+  }
+}
