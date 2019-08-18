d04ad4122a5f38ddfc7a51745300b47ddae58fc3
VaughnVernon <vaughn@forcomprehension.com>
2019-08-17 18:28:26 -0700

    Renamed PersistentObject to StateObject.

--- src/test/java/io/vlingo/lattice/model/object/EmployeeState.java
+++ src/test/java/io/vlingo/lattice/model/object/EmployeeState.java
@@ -9,9 +9,9 @@ package io.vlingo.lattice.model.object;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import io.vlingo.symbio.store.object.PersistentObject;
+import io.vlingo.symbio.store.object.StateObject;
 
-public class EmployeeState extends PersistentObject implements Comparable<EmployeeState> {
+public class EmployeeState extends StateObject implements Comparable<EmployeeState> {
   private static final long serialVersionUID = 1L;
 
   private static final AtomicLong identityGenerator = new AtomicLong(0);
