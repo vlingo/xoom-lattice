beac61f1124dbea5009c8e2f6e227739a48fd943
VaughnVernon <vaughn@forcomprehension.com>
2019-08-18 10:29:21 -0700

    Javadoc.

--- src/main/java/io/vlingo/lattice/router/RoutableCommand.java
+++ src/main/java/io/vlingo/lattice/router/RoutableCommand.java
@@ -15,6 +15,12 @@ import io.vlingo.common.Completes;
 import io.vlingo.lattice.grid.Grid;
 import io.vlingo.lattice.model.Command;
 
+/**
+ * A {@code Command} that may be routed through a defined {@code CommandRouter}.
+ * @param <P> the protocol type
+ * @param <C> the command type
+ * @param <A> the answer (outcome) type
+ */
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class RoutableCommand<P,C extends Command,A> extends Command {
   private Class<? extends Actor> actorType;
@@ -24,54 +30,117 @@ public class RoutableCommand<P,C extends Command,A> extends Command {
   private CommandDispatcher<P,C,Completes<A>> handler;
   private Class<P> protocol;
 
+  /**
+   * Answer a new {@code RoutableCommand} that speaks the {@code protocol}.
+   * @param protocol the Class<P> class type of protocol
+   * @return {@code RoutableCommand<P,C,A>}
+   * @param <P> the protocol type
+   * @param <C> the command type
+   * @param <A> the answer/outcome type
+   */
   public static <P,C extends Command,A> RoutableCommand<P,C,A> speaks(final Class<P> protocol) {
     assert(protocol != null);
     return new RoutableCommand<>(protocol);
   }
 
+  /**
+   * Answer myself after assigning my {@code actorType}.
+   * @param actorType the {@code Class<? extends Actor>} of the actor
+   * @return {@code RoutableCommand<P,C,A>}
+   */
   public RoutableCommand<P,C,A> to(final Class<? extends Actor> actorType) {
     assert(actorType != null);
     this.actorType = actorType;
     return this;
   }
 
+  /**
+   * Answer myself after assigning my {@code address}.
+   * @param address the String address
+   * @return {@code RoutableCommand<P,C,A>}
+   */
   public RoutableCommand<P,C,A> at(final String address) {
     assert(address != null);
     this.address = address;
     return this;
   }
 
+  /**
+   * Answer myself after assigning my {@code command}.
+   * @param command the C typed command
+   * @return {@code RoutableCommand<P,C,A>}
+   */
   public RoutableCommand<P,C,A> delivers(final C command) {
     assert(command != null);
     this.command = command;
     return this;
   }
 
+  /**
+   * Answer my {@code command}.
+   * @return C
+   */
   public C command() {
     return command;
   }
 
+  /**
+   * Answer a new copy of myself.
+   * @return RoutableCommand
+   */
+  public RoutableCommand copy() {
+    return new RoutableCommand(protocol, actorType, address, command, answer, handler);
+  }
+
+  /**
+   * Answer my {@code id}.
+   * @return String
+   */
   @Override
   public String id() {
     return (command == null || command.id().isEmpty() ? super.id() : command.id());
   }
 
+  /**
+   * Answer myself after setting my {@code answer}.
+   * @param answer the ANSWER that serves as my means to answer
+   * @return RoutableCommand
+   * @param <PROTOCOL> the protocol type
+   * @param <COMMAND> the command type
+   * @param <ANSWER> the answer/outcome type
+   */
   public <PROTOCOL,COMMAND extends Command,ANSWER> RoutableCommand<PROTOCOL,COMMAND,ANSWER> answers(final Completes<ANSWER> answer) {
     assert(answer != null);
     this.answer = (Completes<A>) answer;
     return (RoutableCommand<PROTOCOL,COMMAND,ANSWER>) this;
   }
 
+  /**
+   * Answer my {@code answer}.
+   * @return {@code Completes<A>}
+   */
   public Completes<A> answer() {
     return answer;
   }
 
+  /**
+   * Answer myself after assigning my {@code handler}.
+   * @param handler the CommandDispatcher to assign as my handler
+   * @return {@code RoutableCommand<PROTOCOL,COMMAND,ANSWER>}
+   * @param <PROTOCOL> the protocol type
+   * @param <COMMAND> the command type
+   * @param <ANSWER> the answer/outcome type
+   */
   public <PROTOCOL,COMMAND extends Command,ANSWER> RoutableCommand<PROTOCOL,COMMAND,ANSWER> handledBy(final CommandDispatcher handler) {
     assert(handler != null);
     this.handler = handler;
     return (RoutableCommand<PROTOCOL,COMMAND,ANSWER>) this;
   }
 
+  /**
+   * Handle my command my means of {@code grid}.
+   * @param grid the Grid to use for command handling
+   */
   public void handleWithin(final Grid grid) {
     check();
     final AddressFactory addressFactory = grid.addressFactory;
@@ -80,6 +149,10 @@ public class RoutableCommand<P,C extends Command,A> extends Command {
         .andThenConsume(actor -> handler.accept(actor, command, answer));
   }
 
+  /**
+   * Handle my command my means of {@code stage}.
+   * @param stage the Stage to use for command handling
+   */
   public void handleWithin(final Stage stage) {
     check();
     final AddressFactory addressFactory = stage.world().addressFactory();
@@ -90,6 +163,21 @@ public class RoutableCommand<P,C extends Command,A> extends Command {
 
   protected RoutableCommand(final Class<P> protocol) {
     this.protocol = protocol;
+  }
+
+  protected RoutableCommand(
+          final Class<P> protocol,
+          final Class<? extends Actor> actorType,
+          final String address,
+          final C command,
+          final Completes<A> answer,
+          final CommandDispatcher handler) {
+    this.protocol = protocol;
+    this.actorType = actorType;
+    this.address = address;
+    this.command = command;
+    this.answer = answer;
+    this.handler = handler;
   }
 
   private void check() {
