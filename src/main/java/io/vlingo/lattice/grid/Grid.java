f544e74e560e81eb217b3355a0f116d495446c24
VaughnVernon <vaughn@forcomprehension.com>
2019-08-19 01:07:12 -0700

    Now extends Stage and is dynamically created.

--- src/main/java/io/vlingo/lattice/grid/Grid.java
+++ src/main/java/io/vlingo/lattice/grid/Grid.java
@@ -15,65 +15,61 @@ import io.vlingo.actors.Address;
 import io.vlingo.actors.AddressFactory;
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.Stage;
+import io.vlingo.actors.World;
 import io.vlingo.common.Completes;
-import io.vlingo.common.identity.IdentityGeneratorType;
 import io.vlingo.lattice.grid.cache.Cache;
 import io.vlingo.lattice.grid.cache.CacheNodePoint;
 import io.vlingo.lattice.grid.hashring.HashRing;
 import io.vlingo.lattice.grid.hashring.HashedNodePoint;
 import io.vlingo.lattice.grid.hashring.MurmurArrayHashRing;
 
-public class Grid {
-  public final AddressFactory addressFactory;
-
+public class Grid extends Stage {
   private final Cache cache;
   private final BiFunction<Integer, String, HashedNodePoint<String>> factory;
   private final HashRing<String> hashRing;
-  private final Stage stage;
 
-  public static Grid grid(final Stage stage, final IdentityGeneratorType type) {
-    return new Grid(stage, type);
+  public Grid(final World world, final AddressFactory addressFactory, final String name) {
+    super(world, addressFactory, name);
+    this.cache = new Cache();
+    this.factory =  (hash, node) -> { return new CacheNodePoint<String>(this.cache, hash, node); };
+    this.hashRing = new MurmurArrayHashRing<>(100, factory);
   }
 
+  @Override
   public <T> T actorFor(final Class<T> protocol, final Definition definition) {
-    return actorFor(protocol, definition, addressFactory.unique());
+    return actorFor(protocol, definition, addressFactory().unique());
   }
 
+  @Override
   public <T> T actorFor(final Class<T> protocol, final Definition definition, final Address address) {
-    if (stage.world().isTerminated()) {
+    if (world().isTerminated()) {
       throw new IllegalStateException("vlingo/lattice: Grid has stopped.");
     }
     if (!address.isDistributable()) {
       throw new IllegalArgumentException("Address is not distributable.");
     }
 
-    final T actor = stage.actorFor(protocol, definition, address);
+    final T actor = super.actorFor(protocol, definition, address);
 
     return actor;
   }
 
+  @Override
   public <T> T actorFor(final Class<T> protocol, final Class<? extends Actor> type, final Object...parameters) {
-    if (stage.world().isTerminated()) {
+    if (world().isTerminated()) {
       throw new IllegalStateException("vlingo/lattice: Grid has stopped.");
     }
 
-    final T actor = stage.actorFor(protocol, Definition.has(type, Arrays.asList(parameters)), addressFactory.unique());
+    final T actor = super.actorFor(protocol, Definition.has(type, Arrays.asList(parameters)), addressFactory().unique());
 
     return actor;
   }
 
+  @Override
   public <T> Completes<T> actorOf(final Class<T> protocol, final Address address) {
     if (!address.isDistributable()) {
       throw new IllegalArgumentException("Address is not distributable.");
     }
-    return stage.actorOf(protocol, address);
-  }
-
-  private Grid(final Stage stage, final IdentityGeneratorType type) {
-    this.stage = stage;
-    this.addressFactory = new GridAddressFactory(type);
-    this.cache = new Cache();
-    this.factory =  (hash, node) -> { return new CacheNodePoint<String>(this.cache, hash, node); };
-    this.hashRing = new MurmurArrayHashRing<>(100, factory);
+    return super.actorOf(protocol, address);
   }
 }
