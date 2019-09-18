8f7010fd78f2a8655c73ca28bddb2151f5759250
VaughnVernon <vaughn@forcomprehension.com>
2019-09-18 00:01:59 -0700

    Support Entry dispatching.

--- src/main/java/io/vlingo/lattice/model/projection/BinaryProjectionDispatcherActor.java
+++ src/main/java/io/vlingo/lattice/model/projection/BinaryProjectionDispatcherActor.java
@@ -7,13 +7,15 @@
 
 package io.vlingo.lattice.model.projection;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.vlingo.symbio.Entry;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.dispatch.Dispatchable;
 
-import java.util.Collection;
-
-public class BinaryProjectionDispatcherActor extends ProjectionDispatcherActor<State<byte[]>>
+public class BinaryProjectionDispatcherActor extends ProjectionDispatcherActor<Entry<?>, State<byte[]>>
     implements ProjectionDispatcher {
 
   public BinaryProjectionDispatcherActor() {
@@ -23,7 +25,7 @@ public class BinaryProjectionDispatcherActor extends ProjectionDispatcherActor<S
   public BinaryProjectionDispatcherActor(final Collection<ProjectToDescription> projectToDescriptions) {
     super(projectToDescriptions);
   }
-  
+
   @Override
   protected boolean requiresDispatchedConfirmation() {
     return true;
@@ -31,10 +33,19 @@ public class BinaryProjectionDispatcherActor extends ProjectionDispatcherActor<S
 
   @Override
   public void dispatch(final Dispatchable<Entry<?>, State<byte[]>> dispatchable) {
-    dispatchable.state().ifPresent(state -> {
+    dispatchable.state().ifPresent(state-> {
       if (hasProjectionsFor(state.metadata.operation)) {
         dispatch(dispatchable.id(), new BinaryProjectable(state, dispatchable.entries(), dispatchable.id()));
       }
     });
+
+    final List<Entry<?>> entries =
+            dispatchable.entries().stream()
+              .filter(entry -> hasProjectionsFor(entry.typeName()))
+              .collect(Collectors.toList());
+
+    if (!entries.isEmpty()) {
+      dispatch(dispatchable.id(), new BinaryProjectable(dispatchable.state().orElse(null), entries, dispatchable.id()));
+    }
   }
 }
