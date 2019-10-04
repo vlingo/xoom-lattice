e738dbdb6ae49b3b8c8539b32271ca3af38487d4
VaughnVernon <vaughn@forcomprehension.com>
2019-10-04 12:18:56 -0700

    Removed submodule in favor of flat root-level repositories.

--- vlingo-lattice-exchange-rabbitmq/rmqbounce.sh
+++
@@ -1,4 +0,0 @@
-# bounce the docker volume vlingo-lattice-exchange-rabbitmq in the docker-compose.yml
-docker-compose -p "dev" down
-docker volume rm vlingo-lattice-exchange-rabbitmq
-docker-compose -p "dev" up -d
--- vlingo-lattice-exchange-rabbitmq/src/test/resources/rabbitmq/rabbit_bounce.sh
+++
@@ -1,5 +0,0 @@
-pushd ~/Projects/vlingo/vlingo-lattice-exchange-rabbitmq
-docker-compose down
-docker volume rm $(docker volume ls -q)
-docker-compose up -d
-popd
