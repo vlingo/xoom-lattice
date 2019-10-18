1ed8ef172a3f79663f97909b15dd0e8702e2ae59
VaughnVernon <vaughn@forcomprehension.com>
2019-10-17 18:12:13 -0700

    Queries may expire.

---
+++ src/main/java/io/vlingo/lattice/grid/spaces/ExpirableQuery.java
@@ -0,0 +1,43 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.grid.spaces;
+
+import java.time.Instant;
+
+import io.vlingo.actors.CompletesEventually;
+import io.vlingo.lattice.grid.spaces.ScheduledScanner.ScheduledScannable;
+
+class ExpirableQuery implements ScheduledScannable<ExpirableQuery>, Comparable<ExpirableQuery> {
+  final CompletesEventually completes;
+  final Instant expiresOn;
+  final Key key;
+  final Period period;
+  final boolean retainItem;
+
+  ExpirableQuery(final Key key, final boolean retainItem, final Instant expiresOn, final Period period, final CompletesEventually completes) {
+    this.key = key;
+    this.retainItem = retainItem;
+    this.expiresOn = expiresOn;
+    this.period = period;
+    this.completes = completes;
+  }
+
+  boolean isMaximumExpiration() {
+    return expiresOn.getEpochSecond() == Instant.MAX.getEpochSecond();
+  }
+
+  @Override
+  public ExpirableQuery scannable() {
+    return this;
+  }
+
+  @Override
+  public int compareTo(final ExpirableQuery other) {
+    return key.compare(key, other.key);
+  }
+}
