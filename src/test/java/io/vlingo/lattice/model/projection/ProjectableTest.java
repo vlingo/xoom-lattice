a59f8cf8c99ea1e4c2862a851ee8ba17b4472e11
VaughnVernon <vaughn@forcomprehension.com>
2019-10-22 13:01:01 -0700

    Test fail safely (no NPE).

---
+++ src/test/java/io/vlingo/lattice/model/projection/ProjectableTest.java
@@ -0,0 +1,53 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.model.projection;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.State.TextState;
+
+public class ProjectableTest {
+
+  @Test
+  public void testThatNoStateFailsSafely() {
+    final Projectable projectable = new TextProjectable(null, Arrays.asList(), "123");
+
+    Assert.assertEquals(-1, projectable.dataVersion());
+    Assert.assertEquals("", projectable.dataId());
+    Assert.assertEquals("", projectable.metadata());
+    Assert.assertFalse(projectable.hasObject());
+    Assert.assertNull(projectable.object());
+    Assert.assertFalse(projectable.optionalObject().isPresent());
+    Assert.assertFalse(projectable.hasState());
+    Assert.assertEquals(-1, projectable.typeVersion());
+  }
+
+  @Test
+  public void testThatStateDoesNotFail() {
+    final Object object = new Object();
+    final Projectable projectable =
+            new TextProjectable(
+                    new TextState("ABC", String.class, 1, "state", 1, Metadata.with(object, "value", "op1")),
+                    Arrays.asList(),
+                    "123");
+
+    Assert.assertEquals(1, projectable.dataVersion());
+    Assert.assertEquals("ABC", projectable.dataId());
+    Assert.assertEquals("value", projectable.metadata());
+    Assert.assertTrue(projectable.hasObject());
+    Assert.assertNotNull(projectable.object());
+    Assert.assertEquals(object, projectable.object());
+    Assert.assertTrue(projectable.optionalObject().isPresent());
+    Assert.assertTrue(projectable.hasState());
+    Assert.assertEquals(1, projectable.typeVersion());
+  }
+}
