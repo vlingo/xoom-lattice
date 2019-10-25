5223cb4566892c6f92954a138af6879a1b328eb4
VaughnVernon <vaughn@forcomprehension.com>
2019-10-24 17:19:27 -0700

    Test becauseOf[] length > 1.

--- src/test/java/io/vlingo/lattice/model/projection/TextProjectableTest.java
+++ src/test/java/io/vlingo/lattice/model/projection/TextProjectableTest.java
@@ -9,10 +9,13 @@ package io.vlingo.lattice.model.projection;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.Test;
 
+import io.vlingo.symbio.BaseEntry.TextEntry;
+import io.vlingo.symbio.Entry;
 import io.vlingo.symbio.Metadata;
 import io.vlingo.symbio.State.TextState;
 
@@ -26,6 +29,25 @@ public class TextProjectableTest {
     final Projectable projectable = new TextProjectable(state, Collections.emptyList(), "p123");
 
     assertEquals("op", projectable.becauseOf()[0]);
+    assertEquals(textState, projectable.dataAsText());
+    assertEquals("123", projectable.dataId());
+    assertEquals(1, projectable.dataVersion());
+    assertEquals("value", projectable.metadata());
+    assertEquals("p123", projectable.projectionId());
+    assertEquals(String.class.getName(), projectable.type());
+    assertEquals(1, projectable.typeVersion());
+  }
+
+  @Test
+  public void testStateWithEventProjectableness() {
+    final String textState = "test-state";
+    final TextState state =
+            new TextState("123", String.class, 1, textState, 1, Metadata.with("value", "op"));
+    final Entry<String> entry = new TextEntry();
+    final Projectable projectable = new TextProjectable(state, Arrays.asList(entry), "p123");
+
+    assertEquals("op", projectable.becauseOf()[0]);
+    assertEquals("java.lang.Object", projectable.becauseOf()[1]);
     assertEquals(textState, projectable.dataAsText());
     assertEquals("123", projectable.dataId());
     assertEquals(1, projectable.dataVersion());
