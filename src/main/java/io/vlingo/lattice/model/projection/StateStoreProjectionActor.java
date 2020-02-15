147137de681358113444b05b0aa3222c8bb6c1e9
VaughnVernon <vaughn@forcomprehension.com>
2020-02-15 17:26:52 +0100

    Support adapters.

--- src/main/java/io/vlingo/lattice/model/projection/StateStoreProjectionActor.java
+++ src/main/java/io/vlingo/lattice/model/projection/StateStoreProjectionActor.java
@@ -13,8 +13,14 @@ import java.util.function.BiConsumer;
 import io.vlingo.actors.Actor;
 import io.vlingo.common.Outcome;
 import io.vlingo.lattice.model.projection.ProjectionControl.Confirmer;
+import io.vlingo.symbio.DefaultTextEntryAdapter;
+import io.vlingo.symbio.DefaultTextStateAdapter;
+import io.vlingo.symbio.Entry;
+import io.vlingo.symbio.EntryAdapter;
 import io.vlingo.symbio.Metadata;
 import io.vlingo.symbio.Source;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.StateAdapter;
 import io.vlingo.symbio.store.Result;
 import io.vlingo.symbio.store.StorageException;
 import io.vlingo.symbio.store.state.StateStore;
@@ -35,19 +41,48 @@ import io.vlingo.symbio.store.state.StateStore.WriteResultInterest;
 public abstract class StateStoreProjectionActor<T> extends Actor
     implements Projection, ReadResultInterest, WriteResultInterest {
 
+  private final EntryAdapter<Source<?>, Entry<?>> entryAdapter;
+  private final StateAdapter<Object, State<?>> stateAdapter;
   private final ReadResultInterest readInterest;
   private final WriteResultInterest writeInterest;
   private final StateStore stateStore;
 
   /**
    * Construct my final state with the {@code StateStore}, which must
-   * be provided by my concrete extenders.
+   * be provided by my concrete extenders. I provide default state and
+   * entry adapters.
    * @param stateStore the StateStore from which previous state is read and merged current state is written
    */
   public StateStoreProjectionActor(final StateStore stateStore) {
+    this(stateStore, defaultTextStateAdapter(), defaultTextEntryAdapter());
+  }
+
+  /**
+   * Construct my final state with the {@code StateStore}, which must
+   * be provided by my concrete extenders, as well as with a
+   * {@code stateAdapter} and a {@code entryAdapter}.
+   * @param stateStore the StateStore from which previous state is read and merged current state is written
+   * @param stateAdapter the {@code StateAdapter<Object, State<?>>} used by my extenders to adapt persistent state
+   * @param entryAdapter the {@code EntryAdapter<Source<?>, Entry<?>>} used by my extenders to adapt persistent entries
+   */
+  public StateStoreProjectionActor(
+          final StateStore stateStore,
+          final StateAdapter<Object, State<?>> stateAdapter,
+          final EntryAdapter<Source<?>, Entry<?>> entryAdapter) {
+
     this.stateStore = stateStore;
+    this.stateAdapter = stateAdapter;
+    this.entryAdapter = entryAdapter;
     this.readInterest = selfAs(ReadResultInterest.class);
     this.writeInterest = selfAs(WriteResultInterest.class);
+  }
+
+  /**
+   * @see io.vlingo.lattice.model.projection.Projection#projectWith(io.vlingo.lattice.model.projection.Projectable, io.vlingo.lattice.model.projection.ProjectionControl)
+   */
+  @Override
+  public void projectWith(final Projectable projectable, final ProjectionControl control) {
+    upsertFor(projectable, control);
   }
 
   /**
@@ -56,6 +91,17 @@ public abstract class StateStoreProjectionActor<T> extends Actor
    * @return T
    */
   protected abstract T currentDataFor(final Projectable projectable);
+
+  /**
+   * Answer the {@code EntryAdapter<S,E>} previously registered by construction.
+   * @param <S> the {@code Source<?>} type
+   * @param <E> the {@code Entry<?>} type
+   * @return {@code EntryAdapter<S,E>}
+   */
+  @SuppressWarnings("unchecked")
+  protected <S extends Source<?>, E extends Entry<?>> EntryAdapter<S,E> entryAdapter() {
+    return (EntryAdapter<S,E>) this.entryAdapter;
+  }
 
   /**
    * Answer the {@code T} result of merging the {@code T}-typed {@code previousData} and {@code currentData},
@@ -69,6 +115,16 @@ public abstract class StateStoreProjectionActor<T> extends Actor
    * @return T
    */
   protected abstract T merge(final T previousData, final int previousVersion, final T currentData, final int currentVersion);
+
+  /**
+   * Answer the {@code StateAdapter<?,ST>} previously registered by construction.
+   * @param <ST> the {@code State<?>} type
+   * @return {@code StateAdapter<?,ST>}
+   */
+  @SuppressWarnings("unchecked")
+  protected <ST extends State<?>> StateAdapter<?,ST> stateAdapter() {
+    return (StateAdapter<?,ST>) this.stateAdapter;
+  }
 
   /**
    * Upsert the {@code projectable} into the {@code StateStore}, which may be a
@@ -128,5 +184,19 @@ public abstract class StateStoreProjectionActor<T> extends Actor
       logger().info("Query state not written for update because: " + cause.getMessage(), cause);
       return cause.result;
     });
+  }
+
+  //==================================
+  // Internal Implementation
+  //==================================
+
+  @SuppressWarnings("unchecked")
+  private static <S extends Source<?>, E extends Entry<?>> EntryAdapter<S, E> defaultTextEntryAdapter() {
+    return new DefaultTextEntryAdapter();
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <S, ST extends State<?>> StateAdapter<S, ST> defaultTextStateAdapter() {
+    return (StateAdapter<S, ST>) new DefaultTextStateAdapter();
   }
 }
