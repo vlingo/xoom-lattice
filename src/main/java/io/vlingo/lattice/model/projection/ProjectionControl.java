96a20dcdb624efaea52e65656605770a9b61340a
VaughnVernon <vaughn@forcomprehension.com>
2020-03-03 23:49:38 -0700

    ProjectionControl.confirmerFor() static method.

--- src/main/java/io/vlingo/lattice/model/projection/ProjectionControl.java
+++ src/main/java/io/vlingo/lattice/model/projection/ProjectionControl.java
@@ -12,20 +12,21 @@ package io.vlingo.lattice.model.projection;
  */
 public interface ProjectionControl {
   /**
+   * Answer the {@code Confirmer} for the given {@code Projectable} through
+   * which confirmation can be performed as a single operation.
+   * @param projectable the Projectable requiring confirmation of completed projections operations
+   * @param control the ProjectionControl
+   * @return Confirmer
+   */
+  static Confirmer confirmerFor(final Projectable projectable, final ProjectionControl control) {
+    return () -> control.confirmProjected(projectable.projectionId());
+  }
+
+  /**
    * Confirms that all projection operations have been completed.
    * @param projectionId the String unique identity of the projection operation
    */
   void confirmProjected(final String projectionId);
-
-  /**
-   * Answer the {@code Confirmer} for the given {@code Projectable} through
-   * which confirmation can be performed as a single operation.
-   * @param projectable the Projectable requiring confirmation of completed projections operations
-   * @return Confirmer
-   */
-  default Confirmer confirmerFor(final Projectable projectable) {
-    return () -> this.confirmProjected(projectable.projectionId());
-  }
 
   /**
    * Defines the functional interface used to confirm the completion of projections operations.
