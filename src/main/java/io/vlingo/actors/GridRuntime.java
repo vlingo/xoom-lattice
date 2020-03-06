4589d91aa914440c910daa3bdffce65922472fbf
VaughnVernon <vaughn@forcomprehension.com>
2020-03-05 21:42:21 -0700

    Initial revision.

---
+++ src/main/java/io/vlingo/actors/GridRuntime.java
@@ -0,0 +1,22 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.actors;
+
+import io.vlingo.lattice.grid.GridNodeBootstrap;
+import io.vlingo.lattice.grid.application.OutboundGridActorControl;
+import io.vlingo.lattice.grid.hashring.HashRing;
+import io.vlingo.wire.node.Id;
+
+public interface GridRuntime {
+  Actor actorAt(Address address);
+  GridNodeBootstrap gridNodeBootstrap();
+  HashRing<Id> hashRing();
+  void nodeJoined(final Id newNode);
+  void setNodeId(final Id nodeId);
+  void setOutbound(final OutboundGridActorControl outbound);
+}
