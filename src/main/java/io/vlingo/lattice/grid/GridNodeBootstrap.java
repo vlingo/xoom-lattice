cebbf5b1c9d03d3a51306b4d546047551ffbeff4
VaughnVernon <vaughn@forcomprehension.com>
2020-03-05 21:46:36 -0700

    Improved and simplified boot.

--- src/main/java/io/vlingo/lattice/grid/GridNodeBootstrap.java
+++ src/main/java/io/vlingo/lattice/grid/GridNodeBootstrap.java
@@ -9,7 +9,6 @@ package io.vlingo.lattice.grid;
 
 import io.vlingo.actors.Grid;
 import io.vlingo.actors.Logger;
-import io.vlingo.actors.World;
 import io.vlingo.cluster.model.Cluster;
 import io.vlingo.cluster.model.ClusterSnapshotControl;
 import io.vlingo.cluster.model.Properties;
@@ -20,30 +19,22 @@ public class GridNodeBootstrap {
   private final Tuple2<ClusterSnapshotControl, Logger> clusterSnapshotControl;
 
   private final GridShutdownHook shutdownHook;
-  private final Grid grid;
 
-  public static GridNodeBootstrap boot(final String nodeName, final io.vlingo.cluster.model.Properties properties) throws Exception {
-    final Grid grid = Grid.start("vlingo-lattice-grid", nodeName);
-    final GridNodeBootstrap gridNodeBootstrap = boot(grid.world(), grid, nodeName, properties, false);
-    grid.gridNodeBootstrap(gridNodeBootstrap);
-    return gridNodeBootstrap;
+  public static GridNodeBootstrap boot(final Grid grid, final String nodeName, final boolean embedded) throws Exception {
+    return boot(grid, nodeName, io.vlingo.cluster.model.Properties.instance, embedded);
   }
 
-  public static GridNodeBootstrap boot(final World world, final Grid grid, final String nodeName, final boolean embedded) throws Exception {
-    return boot(world, grid, nodeName, io.vlingo.cluster.model.Properties.instance, embedded);
-  }
-
-  public static GridNodeBootstrap boot(final World world, final Grid grid, final String nodeName, final io.vlingo.cluster.model.Properties properties, final boolean embedded) throws Exception {
+  public static GridNodeBootstrap boot(final Grid grid, final String nodeName, final io.vlingo.cluster.model.Properties properties, final boolean embedded) throws Exception {
     Properties.instance.validateRequired(nodeName);
 
     final Tuple2<ClusterSnapshotControl, Logger> control =
             Cluster.controlFor(
-                    world,
+                    grid.world(),
                     new GridNodeInstantiator(grid),
                     properties,
                     nodeName);
 
-    final GridNodeBootstrap instance = new GridNodeBootstrap(control, nodeName, grid);
+    final GridNodeBootstrap instance = new GridNodeBootstrap(control, nodeName);
 
     control._2.info("Successfully started cluster node: '" + nodeName + "'");
 
@@ -58,13 +49,8 @@ public class GridNodeBootstrap {
     return clusterSnapshotControl._1;
   }
 
-  public Grid grid() {
-    return grid;
-  }
-
-  private GridNodeBootstrap(final Tuple2<ClusterSnapshotControl, Logger> control, final String nodeName, final Grid grid) throws Exception {
+  private GridNodeBootstrap(final Tuple2<ClusterSnapshotControl, Logger> control, final String nodeName) throws Exception {
     this.clusterSnapshotControl = control;
-    this.grid = grid;
 
     this.shutdownHook = new GridShutdownHook(nodeName, control);
     this.shutdownHook.register();
