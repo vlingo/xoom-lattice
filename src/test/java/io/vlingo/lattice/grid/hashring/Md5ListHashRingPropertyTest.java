ff102483e7644da97b82dee129769e08548a4ae6
VaughnVernon <vaughn@forcomprehension.com>
2020-03-05 21:51:34 -0700

    Copyright notice.

--- src/test/java/io/vlingo/lattice/grid/hashring/Md5ListHashRingPropertyTest.java
+++ src/test/java/io/vlingo/lattice/grid/hashring/Md5ListHashRingPropertyTest.java
@@ -1,8 +1,15 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.lattice.grid.hashring;
 
-import org.junit.Ignore;
-
 import java.util.function.BiFunction;
+
+import org.junit.Ignore;
 
 @Ignore
 public class Md5ListHashRingPropertyTest extends HashRingPropertyTest {
