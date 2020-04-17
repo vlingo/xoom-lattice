754d9f53d46510d217b09d1332fdade8175e879e
VaughnVernon <vaughn@forcomprehension.com>
2020-04-16 23:27:55 -0700

    Extend UUIDAddress.

--- src/main/java/io/vlingo/actors/GridAddress.java
+++ src/main/java/io/vlingo/actors/GridAddress.java
@@ -10,69 +10,12 @@ package io.vlingo.actors;
 import java.io.Serializable;
 import java.util.UUID;
 
-public final class GridAddress implements Address, Serializable {
+public final class GridAddress extends UUIDAddress implements Serializable {
   private static final long serialVersionUID = -7172480689137574451L;
-
-  private final UUID id;
-  private final String name;
-
-  @Override
-  public long id() {
-    return id.getLeastSignificantBits();
-  }
-
-  @Override
-  public long idSequence() {
-    return id.timestamp();
-  }
-
-  @Override
-  public String idSequenceString() {
-    return "" + id.timestamp();
-  }
-
-  @Override
-  public String idString() {
-    return id.toString();
-  }
-
-  @Override
-  @SuppressWarnings("unchecked")
-  public UUID idTyped() {
-    return id;
-  }
-
-  @Override
-  public String name() {
-    return name == null ? idString() :  name;
-  }
 
   @Override
   public boolean isDistributable() {
     return true;
-  }
-
-  @Override
-  public boolean equals(final Object other) {
-    if (other == null || other.getClass() != GridAddress.class) {
-      return false;
-    }
-    return id.equals(((GridAddress) other).id);
-  }
-
-  @Override
-  public int hashCode() {
-    return id.hashCode();
-  }
-
-  @Override
-  public String toString() {
-    return "GridAddress[id=" + id + ", name=" + (name == null ? "(none)" : name) + "]";
-  }
-
-  @Override
-  public int compareTo(final Address other) {
-    return id.compareTo(((GridAddress) other).id);
   }
 
   GridAddress(final UUID reservedId) {
@@ -84,7 +27,6 @@ public final class GridAddress implements Address, Serializable {
   }
 
   GridAddress(final UUID reservedId, final String name, final boolean prefixName) {
-    this.id = reservedId;
-    this.name = name == null ? null : prefixName ? (name + id) : name;
+    super(reservedId, name, prefixName);
   }
 }
