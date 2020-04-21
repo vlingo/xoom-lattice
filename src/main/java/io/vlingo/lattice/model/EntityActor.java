09feb7fdc550ebab209d8d474ee725b71f59e1c4
VaughnVernon <vaughn@forcomprehension.com>
2020-04-21 00:44:45 -0700

    Initial revision.

---
+++ src/main/java/io/vlingo/lattice/model/EntityActor.java
@@ -0,0 +1,21 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.lattice.model;
+
+import io.vlingo.actors.Actor;
+
+/**
+ * Abstract base of all entity types.
+ */
+public abstract class EntityActor extends Actor {
+
+  /**
+   * Restore my state.
+   */
+  protected abstract void restore();
+}
