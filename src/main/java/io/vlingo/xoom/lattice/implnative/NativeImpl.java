e7a19978db2ca14e7643342aa1e4e0422cf780ef
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-11 07:58:51 +0100

    Adding GraalVM Dashboard Support..

---
+++ src/main/java/io/vlingo/xoom/lattice/implnative/NativeImpl.java
@@ -0,0 +1,35 @@
+package io.vlingo.xoom.lattice.implnative;
+
+import io.vlingo.xoom.actors.Configuration;
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.actors.plugin.logging.slf4j.Slf4jLoggerPlugin;
+import io.vlingo.xoom.cluster.ClusterProperties;
+import io.vlingo.xoom.lattice.grid.Grid;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+
+public final class NativeImpl {
+  @CEntryPoint(name = "Java_io_vlingo_xoom_latticenative_Native_start")
+  public static int start(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
+    final String nameString = CTypeConversion.toJavaString(name);
+    Configuration configuration =
+        Configuration
+            .define()
+            .with(Slf4jLoggerPlugin
+                .Slf4jLoggerPluginConfiguration
+                .define()
+                .defaultLogger()
+                .name("xoom-actors"));
+    World world = World.start(nameString, configuration).world();
+
+    final io.vlingo.xoom.cluster.model.Properties properties = ClusterProperties.oneNode();
+
+    try {
+      Grid.start(world, properties, "node1").quorumAchieved();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return 0;
+  }
+}
