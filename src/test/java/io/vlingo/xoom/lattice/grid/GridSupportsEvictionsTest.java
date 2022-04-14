f2d67d6b1d7d6748a5943a507dbc115942e9efd8
VaughnVernon <vaughn@kalele.io>
2022-04-13 20:00:29 -0700

    Test Stage actor evictions support.

---
+++ src/test/java/io/vlingo/xoom/lattice/grid/GridSupportsEvictionsTest.java
@@ -0,0 +1,31 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.lattice.grid;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.cluster.ClusterProperties;
+
+public class GridSupportsEvictionsTest {
+
+  @Test
+  public void testSupportsEvictions() throws Exception {
+    final World world = World.startWithDefaults("test-world");
+
+    final io.vlingo.xoom.cluster.model.Properties properties = ClusterProperties.oneNode();
+
+    final Grid grid = Grid.start(world, properties, "node1");
+    
+    assertFalse(world.stage().supportsEvictions());
+    assertTrue(grid.supportsEvictions());
+  }
+}
