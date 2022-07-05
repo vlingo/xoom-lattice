02da8a270ccc805d048edb43aa26134ad47422a3
VaughnVernon <vaughn@kalele.io>
2022-07-05 13:34:52 -0700

    Initial revision of MessageTypeNameAdapter and MessageTypeNameAdapterTest

---
+++ src/main/java/io/vlingo/xoom/lattice/exchange/MessageTypeNameAdapter.java
@@ -0,0 +1,43 @@
+// Copyright © 2012-2022 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.lattice.exchange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Adapts internal message type names to a public type name. If given
+ * a {@code startsWithReplacements} constructor parameter, uses the
+ * {@code key} to match the internal type name prefix and replace it
+ * with the {@code value}.
+ */ 
+public class MessageTypeNameAdapter {
+  private final Map<String, String> startsWithReplacements;
+
+  public MessageTypeNameAdapter(Map<String, String> startsWithReplacements) {
+    this.startsWithReplacements = startsWithReplacements == null ? new HashMap<>(0) : startsWithReplacements;
+  }
+
+  /**
+   * Answer the public type name from the internal type name using
+   * my {@code startsWithReplacements} map.
+   * @param internalTypeName the String name of the internal type
+   * @return String
+   */
+  public String toPublicTypeName(final String internalTypeName) {
+    for (final String prefix : startsWithReplacements.keySet()) {
+      if (internalTypeName.startsWith(prefix)) {
+        final String replacement = startsWithReplacements.get(prefix);
+        final String postFix = internalTypeName.substring(prefix.length());
+        final String publicTypeName = replacement + postFix;
+        return publicTypeName;
+      }
+    }
+    return internalTypeName;
+  }
+}
---
+++ src/test/java/io/vlingo/xoom/lattice/exchange/MessageTypeNameAdapterTest.java
@@ -0,0 +1,60 @@
+// Copyright © 2012-2022 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.lattice.exchange;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageTypeNameAdapterTest {
+
+  private static final String INTERNAL_TYPE_NAME = "io.vlingo.xoom.test.one";
+  private static final String PUBLIC_TYPE_NAME = "vlingo.xoom.test";
+  private static final String SIMPLE_TYPE = ".SomeEvent";
+
+  private static final String DOT_INTERNAL_TYPE_NAME = "io.vlingo.xoom.test.two.";
+  private static final String DOT_PUBLIC_TYPE_NAME = "vlingo.xoom.test.";
+  private static final String DOT_SIMPLE_TYPE = "SomeEvent";
+  
+  private static final String UNMATCHED_TYPE_NAME = "io.vlingo.xoom.test.unmatched.Type";
+  
+  private MessageTypeNameAdapter adapter;
+  private Map<String, String> internalToPublic;
+
+  @Test
+  public void testThatMatchedTypeNamesRenamed() {
+    final String publicTypeName = adapter.toPublicTypeName(INTERNAL_TYPE_NAME + SIMPLE_TYPE);
+
+    assertEquals(PUBLIC_TYPE_NAME + SIMPLE_TYPE, publicTypeName);
+
+    final String dotPublicTypeName = adapter.toPublicTypeName(DOT_INTERNAL_TYPE_NAME + DOT_SIMPLE_TYPE);
+
+    assertEquals(DOT_PUBLIC_TYPE_NAME + DOT_SIMPLE_TYPE, dotPublicTypeName);
+  }
+
+  @Test
+  public void testThatUnmatchedTypeNameNotRenamed() {
+    final String publicTypeName = adapter.toPublicTypeName(UNMATCHED_TYPE_NAME);
+
+    assertEquals(UNMATCHED_TYPE_NAME, publicTypeName);
+  }
+
+  @Before
+  public void setUp() {
+    internalToPublic = new HashMap<>();
+
+    internalToPublic.put(INTERNAL_TYPE_NAME, PUBLIC_TYPE_NAME);
+    internalToPublic.put(DOT_INTERNAL_TYPE_NAME, DOT_PUBLIC_TYPE_NAME);
+
+    adapter = new MessageTypeNameAdapter(internalToPublic);
+  }
+}
